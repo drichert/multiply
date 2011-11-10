778788a17c1f84ab2e19edd47816f618f1bcc6d9
Dan Richert <dan.richert@gmail.com>
2011-11-10 00:00:42 -0500

    Multiply module

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -2,4 +2,10 @@ module Multiply
   VERSION = "0.0.1"
 
   autoload :WrapArray, 'multiply/wrap_array'
+
+  class << self
+    def base_path
+      File.expand_path("../..", __FILE__)
+    end
+  end
 end
--- spec/multiply_spec.rb
+++ spec/multiply_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Multiply do
+  describe "::VERSION" do
+    it "should be a valid SemVer version string" do
+      Multiply::VERSION.should match(/^(\d+\.){2}\d+([a-z](\w+)?)?$/)
+    end
+  end
+
+  describe "::base_path" do
+    it "should return the absolute root path of the application" do
+      Multiply::base_path.should == File.expand_path("../..", __FILE__)
+    end
+  end
 end
