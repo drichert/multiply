eb38f9bd5070ff66ba46fef4ca27c65e3dff97fb
Dan Richert <dan.richert@gmail.com>
2011-11-12 08:59:45 -0500

    Move text_* helper methods to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,26 @@
 $:.unshift(File.expand_path(__FILE__, "../lib"))
 
 require 'multiply'
+
+
+def text_strings
+  [
+    "This is a long text. This text is longer than the short text.",
+    "This text is shorter than the long text."
+  ]
+end
+
+def text_arrays
+  [
+    ["This", "is", "a", "long", "text.", "This", "text",
+   "is", "longer", "than", "the", "short", "text."],
+    ["This", "text", "is", "shorter", "than", "the", "long", "text."]
+  ]
+end
+
+def text_files
+  [
+    File.open(File.expand_path("../../share/texts/test/short.txt", __FILE__)),
+    File.open(File.expand_path("../../share/texts/test/long.txt", __FILE__))
+  ]
+end
